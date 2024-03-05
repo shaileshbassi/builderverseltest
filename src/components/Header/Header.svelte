86d39ce57726ad9dacf121c6c5e17ad0a6acd5c5
Anivesh00 <singhanivesh0@gmail.com>
2024-03-05 17:48:28 +0530

    Header added

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let logoName: any;
+  export let image: any;
+  export let navLinks: any;
+</script>
+
+<header class="text-gray-600 body-font">
+  <div
+    class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
+  >
+    <div
+      class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+    >
+      <a href="/" class="flex items-center">
+        <img src={image} class="h-12 w-14" alt="logo" />
+        <span class="ml-3 text-xl">{logoName}</span>
+      </a>
+    </div>
+    <nav
+      class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center"
+    >
+      {#each navLinks as nav}
+        <a href={nav.url} class="mr-5 hover:text-gray-900">{nav.title}</a>
+      {/each}
+    </nav>
+  </div>
+</header>
--- src/registerComponents/componentsRegistry.js
+++ src/registerComponents/componentsRegistry.js
@@ -2,9 +2,11 @@
   // @ts-ignore
 
   import Carousel from "../components/Carousel/carousel.svelte";
+  // @ts-ignore
   import CardSlider from '../components/CardSlider/SliderCard.svelte'
-  import TabSection from "../components/TabComponent/TabSection.svelte";
-
+  // @ts-ignore
+  import Header from '../components/Header/Header.svelte'
+  
   export const CUSTOM_COMPONENTS = [
     {
       component: Carousel,
@@ -79,120 +81,31 @@
       ],
     },
     {
-      component: TabSection,
-      name: "Tab Section",
+      component: Header,
+      name: 'Header',
       inputs: [
         {
-          name: "title",
-          type: "string",
+          name: 'logoName',
+          type: 'string',
         },
         {
-          name:"type",
-          type:"enum",
-          enum: ['Horizontal', 'Vertical']
+          name:'image',
+          type: "file",
         },
         {
-          name: "description",
-          type: "string",
-          showIf:  `options.get('type') === 'Vertical'`,
-        },
-        {
-          name: "buttonText",
-          type: "string",
-          showIf:  `options.get('type') === 'Vertical'`,
-        },
-        {
-          name: "buttonLink",
-          type: "string",
-          showIf:  `options.get('type') === 'Vertical'`,
-        },
-        {
-          name: "verticalTabItems",
-          type: "list",
-          showIf:  `options.get('type') === 'Vertical'`,
+          name:'navLinks',
+          type:'list',
           subFields: [
             {
-              name: "tabTitle",
-              type: "string",
+              name:'title',
+              type:'string'
             },
             {
-              name: "tabHeading",
-              type: "string",
-            },
-            {
-              name: "tabDescription",
-              type: "string",
-            },
-            {
-              name: "tabVideoOrImage",
-              type: "file",
-              allowedFileTypes: ["jpeg", "png", "jpg", "svg", "mp4"],
-            },
-            {
-              name: "tabButtonText",
-              type: "string",
-            },
-            {
-              name: "tabButtonLink",
-              type: "string",
-            },
-            {
-              name: "tabCardItem",
-              type: "list",
-              subFields: [
-                {
-                  name: "tabCardTitle",
-                  type: "string",
-                },
-                {
-                  name: "tabCardDescription",
-                  type: "string",
-                },
-                {
-                  name: "tabCardImage",
-                  type: "file",
-                  allowedFileTypes: ["jpeg", "png", "jpg", "svg"],
-                },
-                {
-                  name: "tabCardLink",
-                  type: "string",
-                },
-              ],
-            },
-          ],
-        },
-        {
-          name: "horizontalTabItems",
-          type: "list",
-          showIf:  `options.get('type') === 'Horizontal'`,
-          subFields: [
-            {
-              name: "tabTitle",
-              type: "string",
-            },
-            {
-              name: "cardHeading",
-              type: "string",
-            },
-            {
-              name: "cardDescription",
-              type: "string",
-            },
-            {
-              name: "cardImage",
-              type: "file",
-              allowedFileTypes: ["jpeg", "png", "jpg", "svg"],
-            },
-            {
-              name: "cardButtonText",
-              type: "string",
-            },
-            {
-              name: "cardButtonUrl",
-              type: "string",
-            },
-          ],
-        },
-      ],
-    },
+              name:'url',
+              type:'string'
+            }
+          ]
+        }
+      ]
+    }
   ];
